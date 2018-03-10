c7aef3f66860ced174833b365cd18023cad3de07
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2018-03-10 18:21:06 +0100

    new proj

--- Rawdata2/Program.cs
+++ Rawdata2/Program.cs
@@ -1,12 +1,115 @@
-﻿using System;
+﻿using DomainModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
-namespace Rawdata2
+namespace EchoServer
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var addr = IPAddress.Parse("127.0.0.1");
+            var server = new TcpListener(addr, 5000);
+
+            server.Start();
+            Console.WriteLine("Server started ...");
+
+
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+                new Thread(() => ProcessData(client)).Start();
+
+            }
+
+            //server.Stop();
+
+        }
+
+        public static void ProcessData(TcpClient client)
+        {
+            var strm = client.GetStream();
+
+            var buffer = new byte[client.ReceiveBufferSize];
+            var readCnt = strm.Read(buffer, 0, buffer.Length);
+
+            var payload = Encoding.UTF8.GetString(buffer, 0, readCnt);
+            var request = JsonConvert.DeserializeObject<Request>(payload);
+
+            Console.WriteLine(request.Body);
+            Console.WriteLine("Soo far");
+            var res = Encoding.UTF8.GetBytes(GetStatus(request));
+
+            strm.Write(res, 0, res.Length);
+        }
+
+        public static string GetStatus(Request request)
+        {
+            var result = new Response();
+
+
+            //check method
+            switch (request.Method)
+            {
+                case "create":
+                    result.Status = "2 ok";
+                    break;
+                case "read":
+                    result.Status = "2 ok";
+                    break;
+                case "update":
+                    result.Status = "2 ok";
+                    break;
+                case "delete":
+                    result.Status = "2 ok";
+                    break;
+                case "echo":
+                    result.Status = "2 ok";
+                    break;
+                case "":
+                    result.Status += "4 missing method";
+                    break;
+                default:
+                    result.Status += "4 illegal method";
+                    break;
+            }
+
+
+            // /[category]/[resource] 
+            var path = request.Path.Split('/');
+            if (!String.IsNullOrEmpty(request.Path))
+            {
+                result.Status += "4 missing path,";
+            }
+            else if (request.Path.Contains(" ") || path.Length < 1 || path.Length > 2)
+            {
+                result.Status += "4 illegal path,";
+            }
+            else if (path.Length == 1)
+            {
+                result.Status += "4 missing resource,";
+            }
+            else if (path.Length == 2 && String.IsNullOrEmpty(path[1]))
+            {
+                result.Status = "2 ok,";
+            }
+
+
+            return ToJson(result);
+
+        }
+
+        public static string ToJson(this object data)
+        {
+            return JsonConvert.SerializeObject(data,
+            new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
         }
     }
 }
